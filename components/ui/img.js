a0a7240233a27cab9f67a038138297b4d3746719
nazip <nazipm@mail.ru>
2016-12-22 12:23:42 +0300

    clean

--- components/ui/img.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-class Img extends React.Component {
-  render() {
-    return React.DOM.img(this.props.imgAttr);
-  }
-};
-export default Img;
--- components/ui/textBox.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-class TextBox extends React.Component {
-  render() {
-    return <span>{this.props.txt}</span>
-  }
-};
-export default TextBox;
